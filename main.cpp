f0e15fec08aa3d1f8cf5447031763db6b564a8db
Nielark <markdanielmagpantay2001@gmail.com>
2023-01-18 19:49:33 +0800

    Added some conditions and comments also makes the code cleaner

--- main.cpp
+++ main.cpp
@@ -7,23 +7,43 @@ using namespace std;
 
 int main()
 {
-    const char allCharacters[] = ";,:.+-*/%><=!~^&|[]{}()0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const char letterLower[] = "abcdefghijklmnopqrstuvwxyz";
-    const char letterUppers[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const char numbers[] = "0123456789";
-    const char symbols[] = ";,:.+-*/%><=!~^&|[]{}()";
-    int passwordLen;
-    srand(time(0));
+    srand(time(0)); // Random seed.
+    const string specialKeys = ";,:.+-*/%><=!~^&|[]{}()";     // 23 Special Characters
+    const string numbers = "0123456789";                      // 10 Numbers
+    const string letterLower = "abcdefghijklmnopqrstuvwxyz";  // 26 Lowercase Letters
+    const string letterUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  // 26 Uppercase Letters
+    const int specKeySize = 23, numberSize = 10, letterLowerSize = 26, letterUpperSize =26;
+    int totalChar, passwordLen;
+    totalChar = specKeySize + numberSize + letterLowerSize + letterUpperSize;
+    string characters = specialKeys + numbers + letterLower + letterUpper; // Concatenating the strings.
+    string password;
 
-    cout << "P A S W O R D  G E N E R A T O R\n\n";
-
+    top:
+    cout << "P A S S W O R D  G E N E R A T O R\n\n";
+    cout << ">> Note: To exit the program just input 0. <<\n\n";
     cout << "Enter the length for the password: ";
     cin >> passwordLen;
+    cout << endl;
 
-    for(int i = 0; i < passwordLen; i++){
-        int randNum = 0 + (rand() % 70);
-        cout << characters[randNum];
+    if(passwordLen >= 8 && passwordLen != 0){
+        for(int i = 0; i < passwordLen; i++){
+            int randNum = 0 + (rand() % totalChar - 1); // Random number generator from 0 up to the totalChar.
+            password += characters[randNum];
+        }
+
+        cout << "Generated Password: " << password << endl;
+    }
+    else if(passwordLen == 0){
+        return 0; // terminate the program.
+    }
+    else{
+        cout << "Password length must be eight(8) characters or greater.\n";
     }
 
-    return 0;
+    cout << endl;
+
+    system("PAUSE");
+    system("CLS");
+    password = ""; // Initialize back to NULL.
+    goto top;
 }
